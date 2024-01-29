0e1afc96a928d4af8230c461db10ff5c712682a4
BUUSHABR <98080399+BUUSHABR@users.noreply.github.com>
2024-01-29 12:20:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import React from 'react';
+import {Provider} from 'react-redux';
+import Store from "./src/store/store"  
+import {name as appName} from './app.json';
+
+const Root = () => (
+  <Provider store={Store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Root);
